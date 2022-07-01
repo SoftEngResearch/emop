265242899a7308c5140d350423e8d4d0e53825ad
Pengyue Jiang <pengyue3@illinois.edu>
2022-07-01 16:18:45 -0500

    Implement class to specs map in-memory storage and file output

--- emop-maven-plugin/src/main/java/edu/cornell/AffectedSpecsMojo.java
+++ emop-maven-plugin/src/main/java/edu/cornell/AffectedSpecsMojo.java
@@ -1,16 +1,24 @@
 package edu.cornell;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.JarURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
@@ -26,6 +34,13 @@ import org.aspectj.tools.ajc.Main;
 
 @Mojo(name = "affected-specs", requiresDirectInvocation = true, requiresDependencyResolution = ResolutionScope.TEST)
 public class AffectedSpecsMojo extends ImpactedClassMojo {
+
+    public static final int CLASS_IDX_IN_MSG = 3;
+    public static final int SPEC_IDX_IN_MSG = 5;
+    public static final int TRIMMED_SPEC_NAME_IDX = 4;
+
+    private Map<String, Set<String>> classToSpecs = new HashMap<>();
+
     public void execute() throws MojoExecutionException {
         super.execute();
         getLog().info("[eMOP] Invoking the AffectedSpecs Mojo...");
@@ -34,9 +49,90 @@ public class AffectedSpecsMojo extends ImpactedClassMojo {
         MessageHandler mh = new MessageHandler();
         compiler.run(arguments, mh);
         IMessage[] ms = mh.getMessages(IMessage.WEAVEINFO, false);
+        computeMapFromMessage(ms);
+        writeMapToFile("txt");
         Writer.writeToFile(Arrays.asList(ms), getArtifactsDir() + File.separator + "join-points");
         getLog().info("[eMOP] Number of impacted classes: " + getImpacted().size());
         getLog().info("[eMOP] Number of messages to process: " + Arrays.asList(ms).size());
+    }
+
+    /**
+     * Compute a mapping from affected classes to specifications based on the messages from AJC.
+     * TODO: Handling output messages are definitely slower than reading them directly from AJC
+     * @param ms An array of IMessage objects
+     */
+    private void computeMapFromMessage(IMessage[] ms) throws MojoExecutionException {
+        for (IMessage message : ms) {
+            String[] lexedMessage = message.getMessage().split("'");
+            String key = lexedMessage[CLASS_IDX_IN_MSG];
+            String value = lexedMessage[SPEC_IDX_IN_MSG].substring(TRIMMED_SPEC_NAME_IDX);
+            if (!classToSpecs.containsKey(key)) {
+                classToSpecs.put(key, new HashSet<>());
+            }
+            classToSpecs.get(key).add(value);
+        }
+    }
+
+    /**
+     * Write map from class to specs in either text or binary format.
+     * @param format Output format of the map, text or binary
+     */
+    private void writeMapToFile(String format) throws MojoExecutionException {
+        if (format.equals("bin")) {
+            // Referenced from https://www.geeksforgeeks.org/how-to-serialize-hashmap-in-java/
+            try {
+                FileOutputStream fos = new FileOutputStream(getArtifactsDir() + File.separator + "classToSpecs.bin");
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+                oos.writeObject(classToSpecs);
+                oos.close();
+                fos.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            try {
+                FileWriter writer = new FileWriter(getArtifactsDir() + File.separator + "classToSpecs.txt");
+                for (Map.Entry<String, Set<String>> entry : classToSpecs.entrySet()) {
+                    String key = entry.getKey();
+                    Set<String> value = entry.getValue();
+                    writer.write(key);
+                    Iterator<String> it = value.iterator();
+                    boolean first = true;
+                    while (it.hasNext()) {
+                        if (first) {
+                            writer.write(":");
+                            first = false;
+                        } else {
+                            writer.write(",");
+                        }
+                        writer.write(it.next());
+                    }
+                    writer.write("\n");
+                }
+                writer.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Reads the binary file that stores the map.
+     * @return The map read from file
+     */
+    private Map<String, Set<String>> readMapFromFile() throws MojoExecutionException {
+        // Referenced from https://www.geeksforgeeks.org/how-to-serialize-hashmap-in-java/
+        Map<String, Set<String>> map = new HashMap<>();
+        try {
+            FileInputStream fileInput = new FileInputStream(getArtifactsDir() + File.separator + "classToSpecs.bin");
+            ObjectInputStream objectInput = new ObjectInputStream(fileInput);
+            map = (Map) objectInput.readObject();
+            objectInput.close();
+            fileInput.close();
+        } catch (IOException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        return map;
     }
 
     private String[] createAJCArguments() throws MojoExecutionException {
