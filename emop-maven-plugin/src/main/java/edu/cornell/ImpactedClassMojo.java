71b8b7078ab60881bdc7516fc4e63670a0e87207
Pengyue Jiang <pengyue3@illinois.edu>
2022-07-07 16:54:16 -0500

    Make updateImpactedChecksums to be true by default

--- emop-maven-plugin/src/main/java/edu/cornell/ImpactedClassMojo.java
+++ emop-maven-plugin/src/main/java/edu/cornell/ImpactedClassMojo.java
@@ -3,11 +3,20 @@ package edu.cornell;
 import edu.illinois.starts.jdeps.ImpactedMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 
 @Mojo(name = "impacted", requiresDirectInvocation = true, requiresDependencyResolution = ResolutionScope.TEST)
 public class ImpactedClassMojo extends ImpactedMojo {
+
+    /**
+     * Parameter to determine whether file checksums are updated.
+     */
+    @Parameter(property = "updateChecksums", defaultValue = "true")
+    private boolean updateChecksums;
+
     public void execute() throws MojoExecutionException {
+        setUpdateImpactedChecksums(updateChecksums);
         getLog().info("[eMOP] Invoking the ImpactedClasses Mojo...");
         super.execute();
     }
