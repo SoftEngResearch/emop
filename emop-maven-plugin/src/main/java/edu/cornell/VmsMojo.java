3d6ff7a8856ef2ca78196d750b57f80e54fe3f2d
Valeria Marques <vmm49@cornell.edu>
2022-10-20 23:18:18 -0400

    Add JGit into project

--- emop-maven-plugin/src/main/java/edu/cornell/VmsMojo.java
+++ emop-maven-plugin/src/main/java/edu/cornell/VmsMojo.java
@@ -1,15 +1,21 @@
 package edu.cornell;
 
+import java.io.File;
+
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Execute;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 
 @Mojo(name = "vms", requiresDirectInvocation = true, requiresDependencyResolution = ResolutionScope.TEST)
 @Execute(phase = LifecyclePhase.TEST, lifecycle = "vms")
 public class VmsMojo extends MonitorMojo {
     public void execute() throws MojoExecutionException {
         getLog().info("[eMOP] Invoking the VMS Mojo...");
+        getLog().info("I think the artifacts directory is here:" + getArtifactsDir());
+        Git git = null;
     }
 }
