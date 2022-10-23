4fbaea0add19ce530c4fc592bc3191ad7f1d6362
Ayaka Yorihiro <ay436@cornell.edu>
2022-10-23 00:09:46 -0400

    Adjustments on javamop installation script

--- scripts/make-and-install-javamop-agent.sh
+++ scripts/make-and-install-javamop-agent.sh
@@ -7,8 +7,7 @@ AGENT_DIR=${SCRIPT_DIR}/javamop-agent-bundle
 cd ${SCRIPT_DIR}
 
 if [ ! -d ${AGENT_DIR} ]; then
-    wget https://www.cs.cornell.edu/courses/cs6156/2020fa/resources/javamop-agent-bundle.tgz
-    tar -xf javamop-agent-bundle.tgz && rm javamop-agent-bundle.tgz
+    git clone https://github.com/SoftEngResearch/javamop-agent-bundle.git
 fi
 
 bash ${AGENT_DIR}/make-and-install-agent.sh
