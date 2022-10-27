0337ef41f196a78f20b7da1f184db20c3e2d14d7
bcarlet <8906114+bcarlet@users.noreply.github.com>
2022-10-27 12:45:06 -0400

    Update clean mojo

---
+++ emop-maven-plugin/src/main/java/edu/cornell/CleanMojo.java
@@ -0,0 +1,40 @@
+package edu.cornell;
+
+import java.nio.file.Paths;
+
+import edu.illinois.starts.helpers.FileUtil;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+@Mojo(name = "clean", requiresDirectInvocation = true)
+public class CleanMojo extends edu.illinois.starts.jdeps.CleanMojo {
+
+    /**
+     * Options for which artifacts to clean.
+     */
+    public static enum CleanMode {
+        EVERYTHING,     // All artifacts
+        RVM             // RV-Monitor artifacts only
+    }
+
+    /**
+     * Parameter to determine which artifacts to clean.
+     */
+    @Parameter(property = "clean.mode", defaultValue = "EVERYTHING")
+    private CleanMode mode;
+
+    public void execute() throws MojoExecutionException {
+        switch (mode) {
+            case EVERYTHING:
+                super.execute();
+                FileUtil.delete(Paths.get(System.getProperty("user.dir"), "weaved-bytecode").toFile());
+                // fall through
+            case RVM:
+                FileUtil.delete(Paths.get(System.getProperty("user.dir"), "violation-counts").toFile());
+                break;
+            default:
+                break;
+        }
+    }
+}
