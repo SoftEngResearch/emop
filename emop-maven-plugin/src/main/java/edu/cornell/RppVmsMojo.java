bf5ef9ee4b94c36d274183db91ca2159cc3a96ba
Ayaka Yorihiro <ay436@cornell.edu>
2023-04-18 00:12:32 -0400

    [WIP] RPP-VMS needs to be an actual plugin

--- emop-maven-plugin/src/main/java/edu/cornell/RppVmsMojo.java
+++ emop-maven-plugin/src/main/java/edu/cornell/RppVmsMojo.java
@@ -1,8 +1,14 @@
 package edu.cornell;
 
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 
+@Mojo(name = "rpp-vms", requiresDirectInvocation = true, requiresDependencyResolution = ResolutionScope.TEST)
+@Execute(phase = LifecyclePhase.TEST, lifecycle = "rpp-vms")
 public class RppVmsMojo extends VmsMojo {
 
     @Parameter(property = "demoteCritical", defaultValue = "false", required = false)
