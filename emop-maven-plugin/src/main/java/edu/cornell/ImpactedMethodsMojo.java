a7d99d602d551f3d667e50f934dc8026abdfd66e
Mahdi Khosravi <mahdi.khosravi@metu.edu.tr>
2023-09-08 10:18:55 +0000

    added methods related mojos

---
+++ emop-maven-plugin/src/main/java/edu/cornell/ImpactedMethodsMojo.java
@@ -0,0 +1,39 @@
+package edu.cornell;
+
+import edu.illinois.starts.jdeps.MethodsMojo;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+@Mojo(name = "impacted-methods", requiresDirectInvocation = true, requiresDependencyResolution = ResolutionScope.TEST)
+public class ImpactedMethodsMojo extends MethodsMojo {
+
+    /**
+     * Parameter to determine whether file checksums are updated.
+     */
+    @Parameter(property = "updateChecksums", defaultValue = "true")
+    private boolean updateChecksums;
+
+    /**
+     * Parameter to determine whether file checksums are updated.
+     */
+    @Parameter(property = "computeImpactedMethods", defaultValue = "true")
+    private boolean computeImpactedMethods;
+
+    public void execute() throws MojoExecutionException {
+        setUpdateMethodsChecksums(true);
+        setComputeImpactedMethods(true);
+        long start = System.currentTimeMillis();
+        getLog().info("[eMOP] Invoking the ImpactedMethods Mojo...");
+        super.execute();
+        long end = System.currentTimeMillis();
+        getLog().info("[eMOP Timer] Execute ImpactedMethods Mojo takes " + (end - start) + " ms");
+    }
+
+    public boolean getComputeImpactedMethods() {
+        return computeImpactedMethods;
+    }
+
+}
