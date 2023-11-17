1c9f333c796a70cf4bd8881e027e078ea04cf875
mopuser <jiwu5031@uni.sydney.edu.au>
2023-11-17 10:48:40 +0000

    change it script

--- emop-maven-plugin/src/it/BasicTestExample1/verify.groovy
+++ emop-maven-plugin/src/it/BasicTestExample1/verify.groovy
@@ -1,14 +1,18 @@
 def firstRun = new File(basedir, "first-run.txt");
 
 def logContent = new File(basedir, 'build.log').text
-def matcher = logContent =~ /affectedSpecs: \[(.+?)\]/
+println("SSSSSSSS")
+println(logContent)
+println("SSSSSSS")
+def matcher = logContent =~ /AffectedSpecs: \[(.+?)\]/
+
 def affectedSpecsMessage = matcher[0][1]
 
 if (!firstRun.exists()) {
     firstRun.createNewFile();
     // No expectations for the first run
 } else {
-    println("The affectedSpecs are: " + affectedSpecsMessage)
+    println("The AffectedSpecs are: " + affectedSpecsMessage)
     assert affectedSpecsMessage == "Collections_SynchronizedCollectionMonitorAspect" : "Unexpected affected specs message for subsequent runs!"
     firstRun.delete()
 }
